4273abf06ca1b1a49397b1c7b0682350a3d5e874
ClariceMaciel <claros.maciel@gmail.com>
2018-09-18 20:00:43 -0300

    Atualizando class

--- sql/Artigos.class.php
+++ sql/Artigos.class.php
@@ -9,41 +9,41 @@
 		//private $imagem;
 		private $id_user;
 		private $id_cat;
-		
+
 		private $conexao;
 		private $tabela;
-		
+
 		public function __construct(){
 			$this->conexao = mysqli_connect("127.0.0.1", "root", "", "arquinuv") or die ("Erro ao conectar");
 			mysqli_set_charset($this->conexao,"utf8");
 			$this->tabela = "artigos";
 		}
-		
+
 		public function __destruct(){
 			unset($this->link);
 		}
-		
+
 		public function __get($key){
 		return $this->$key;
 		}
-		
+
 		public function __set($key, $value){
 			$this->$key = $value;
 		}
-		
+
 		public function InserirArquivo(){
-			$sql = "INSERT INTO $this->tabela(titulo, data_pub, num_pag, descricao, arquivo, id_user, id_cat) 
-			values('$this->titulo',$this->data_pub, $this->num_pag, '$this->descricao', '$this->arquivo',$this->id_user,$this->id_cat)";	
+			$sql = "INSERT INTO $this->tabela(titulo, data_pub, num_pag, descricao, arquivo, id_user, id_cat)
+			values('$this->titulo',$this->data_pub, $this->num_pag, '$this->descricao', '$this->arquivo',$this->id_user,$this->id_cat)";
 			$retorno = mysqli_query($this->conexao, $sql);
 			return $retorno;
 		}
-		
+
 		public function ListarArquivo($complemento=""){
 			$sql = "SELECT $this->tabela.*,categoria.nome FROM $this->tabela INNER JOIN categoria ON $this->tabela.id_cat = categoria.id".$complemento;
 			$retorno = mysqli_query($this->conexao, $sql);
-			
+
 			$arrayObj = NULL;
-			
+
 			while($res = mysqli_fetch_assoc($retorno)){
 				$obj = new Artigos();
 				$obj->id = $res["id"];
@@ -53,20 +53,20 @@
 				$obj->descricao = $res["descricao"];
 				$obj->arquivo = $res["arquivo"];
 				//$obj->imagem = $res["imagem"];
-				$obj->id_user = $res["id_user"];	
-				$obj->id_cat = $res["id_cat"];	
-				
+				$obj->id_user = $res["id_user"];
+				$obj->id_cat = $res["id_cat"];
+
 				$arrayObj[] = $obj;
 			}
-			
-			return $arrayObj;		
+
+			return $arrayObj;
 		}
-		
+
 		public function RetornarArquivoUnico(){
 		$sql = "SELECT * FROM $this->tabela where id=$this->id";
 		$retorno = mysqli_query($this->conexao, $sql);
 		$resultado = mysqli_fetch_assoc($retorno);
-		
+
 		if($resultado){
 			$objeto = new Artigos();
 			$objeto->id = $resultado['id'];
@@ -76,20 +76,25 @@
 			$objeto->descricao = $resultado['descricao'];
 			$objeto->arquivo = $resultado['arquivo'];
 			//$objeto->imagem = $resultado['imagem'];
-			$obj->id_user = $res['id_user'];	
+			$obj->id_user = $res['id_user'];
 			$objeto->id_cat = $resultado['id_cat'];
-			
+
 			$ArquivoRetornado = $objeto;
 		}
 		else {
 			$ArquivoRetornado = null;
 		}
-		
+
 		return $ArquivoRetornado;
-			
+
 	}
-		
-		
+
+	public function Excluir(){
+		$sql = "DELETE FROM $this->tabela WHERE id = $this->id";
+		$retorno = mysqli_query($this->conexao, $sql);
+		return $retorno;
+	}
+
 	}
 
 ?>

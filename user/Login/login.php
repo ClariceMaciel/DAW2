fe23a83aae8bb384e07b10e95bca978c21f4139e
ClariceMaciel <claros.maciel@gmail.com>
2018-10-15 11:56:15 -0300

    Interface Login User

--- user/Login/login.php
+++ user/Login/login.php
@@ -1,15 +1,45 @@
 <!DOCTYPE html>
-<html>
-	<head></head>
-	<body>
-		<form method="POST" action="loginok.php">
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
 
-		Email:<input type="text" name="email"/>
-		Senha:<input type="password" name="senha"/>
-		
-		<input type="submit" name="botao"/>
-		</form>
-	</body>
+    <title>Login</title>
+    <link href="../../theme/ample-admin-lite/html/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+    <link href="../../theme/ample-admin-lite/html/less/icons/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link href="../../theme/ample-admin-lite/html/css/style_login_user.css" rel="stylesheet">
+    <link href="../../theme/ample-admin-lite/html/assets/css/style-responsive.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+	  <div id="login-page">
+	  	<div class="container">
+
+		      <form class="form-login" action="loginok.php" method="POST">
+		        <h2 class="form-login-heading">Login</h2>
+		        <div class="login-wrap">
+		            <input type="text" class="form-control" placeholder="User Email" name="email" autofocus required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
+		            <br>
+		            <input type="password" class="form-control" placeholder="Password" name="senha" required><br><br>
+		            <button class="btn btn-theme btn-block" type="submit">Submit</button>
+		            <button class="btn btn-theme btn-block" type="reset">Reset</button>
+		            <hr>
+								<a class="btn btn-theme btn-block" href="../Cadastro/cadastro.html">Realizar Cadastro</a>
+		        </div>
+		      </form>
+
+	  	</div>
+	  </div>
+
+    <script src="../../theme/ample-admin-lite/html/assets/js/jquery.js"></script>
+    <script src="../../theme/ample-admin-lite/html/assets/js/bootstrap.min.js"></script>
+
+    <script type="text/javascript" src="../../theme/ample-admin-lite/html/assets/js/jquery.backstretch.min.js"></script>
+  </body>
 </html>
 
 <?php

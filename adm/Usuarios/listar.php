e2e4738fa192a83f4c398b449311a544887d1c55
ClariceMaciel <claros.maciel@gmail.com>
2018-10-15 19:32:06 -0300

    Actions ADM

--- adm/Usuarios/listar.php
+++ adm/Usuarios/listar.php
@@ -18,6 +18,7 @@
 						<div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
 								<h4 class="page-title">Usuários</h4> </div>
 						<div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+							<a href="inserir.php" target="_blank" class="btn btn-danger pull-right m-l-20 hidden-xs hidden-sm waves-effect waves-light">Inserir Usuário</a>
 								<ol class="breadcrumb">
 										<li><a href="../Index/index.php">Dashboard</a></li>
 										<li class="active">Usuários</li>
